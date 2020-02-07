7b3c46ac4dc4fc4fbd08cfab2cc3f67edf3e612f
Mony SOEURN <soeurn.mony@gmail.com>
2020-02-07 11:39:52 +0700

    [TASK] finish create backend, middleware, server and front-end

---
+++ README.md
@@ -0,0 +1,9 @@
+# mern_todo
+
+Repository Here, i am practising a CRUD application using MERN(MongoDB, Express, Reactjs, and Nodejs).
+For user to unable to run the project, they have to do the following step:
+
+1. Clone the project:
+  - 
+2. Install MongoDB
+3. 
---
+++ mern-todo-app-s.mony/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.png';
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoList from './components/todo-list.component';
+import EditTodo from './components/edit-todo.component';
+import CreateTodo from './components/create-todo.component';
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="https://www.google.com" target="_blank">
+            <img src={logo} width="30px" height="30px" alt="m-logo"/>
+          </a>
+          <Link to="/" className="navbar-brand">Mony-SOEURN Todo App</Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">Todos</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/edit/:id" className="nav-link">Edot-Todos</Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">Create-Todos</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Route path="/" exact component={TodoList}/>
+        <Route path="/edit/:id" component={EditTodo}/>
+        <Route path="/create" component={CreateTodo}/>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
